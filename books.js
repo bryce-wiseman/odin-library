96a0ae882ccde027d51ca9a1e71b9c8068d52f06
bryce-wiseman <119898021+bryce-wiseman@users.noreply.github.com>
2023-10-19 10:10:33 -0400

    completed basic functionality w/ adding/deleting books

---
+++ books.js
@@ -0,0 +1,170 @@
+
+const myShelf = [
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        pages: 310,
+        read: true
+    }, 
+    {
+        title: "Paper Towns",
+        author: "John Green",
+        pages: 305,
+        read: true
+    }, 
+    {
+        title: "Pride and Prejudice",
+        author: "Jane Eyre",
+        pages: 496,
+        read: false
+    },
+    {
+        title: "Fourth Wing",
+        author: "Rebecca Yarros",
+        pages: 528,
+        read: true
+    }
+]
+
+let titleVal = document.querySelector('#title')
+let authorVal = document.querySelector('#author')
+let pagesVal = document.querySelector('#pages')
+let readVal = document.querySelector('#read')
+
+let bookNum = myShelf.length
+
+function displayShelf () {
+    for(i = 0; i < bookNum; i++) {
+        let div = document.createElement("div")
+        div.className = "book"
+        div.id = 'book' + i
+        console.log(div)
+        document.getElementById("shelf").appendChild(div)
+
+        let title = document.createElement("div")
+        title.className = "title"
+        title.innerHTML = myShelf[i].title 
+        document.getElementById('book'+i).appendChild(title)
+
+        let author = document.createElement("div")
+        author.className = "author"
+        author.innerHTML = myShelf[i].author 
+        document.getElementById('book'+i).appendChild(author)
+
+        let pages = document.createElement("div")
+        pages.className = "pages"
+        pages.innerHTML = myShelf[i].pages + " pages"
+        document.getElementById('book'+i).appendChild(pages)
+
+        let read = document.createElement("div")
+        read.className = "read"
+        if (myShelf[i].read == true) {
+            read.innerHTML = 'Read'
+        } else {
+            read.innerHTML = 'Not Read'
+        }
+        document.getElementById('book'+i).appendChild(read)
+
+        let btn = document.createElement("button")
+        btn.className = "delete"
+        btn.id = i
+        btn.innerHTML = 'x'
+        btn.addEventListener('click', () => {
+            let x = Number(btn.id)
+            myShelf.splice(x, 1)
+            console.log(myShelf)
+            bookNum = myShelf.length
+            console.log(bookNum)
+            displayNewShelf()
+        })
+        document.getElementById('book'+i).appendChild(btn)
+    }
+}
+displayShelf()
+
+const shelf = document.getElementById('shelf')
+
+function displayNewShelf() {
+    while (shelf.hasChildNodes()) {
+        shelf.removeChild(shelf.firstChild)
+    }
+    bookNum = myShelf.length
+    for (i = 0; i < bookNum; i++) {
+        let div = document.createElement("div")
+        div.className = "book"
+        div.id = 'book' + i
+        console.log(div)
+        document.getElementById("shelf").appendChild(div)
+
+        let title = document.createElement("div")
+        title.className = "title"
+        title.innerHTML = myShelf[i].title 
+        document.getElementById('book'+i).appendChild(title)
+
+        let author = document.createElement("div")
+        author.className = "author"
+        author.innerHTML = myShelf[i].author 
+        document.getElementById('book'+i).appendChild(author)
+
+        let pages = document.createElement("div")
+        pages.className = "pages"
+        pages.innerHTML = myShelf[i].pages + " pages"
+        document.getElementById('book'+i).appendChild(pages)
+
+        let read = document.createElement("div")
+        read.className = "read"
+        if (myShelf[i].read == true) {
+            read.innerHTML = 'Read'
+        } else {
+            read.innerHTML = 'Not Read'
+        }
+        document.getElementById('book'+i).appendChild(read)
+
+        let btn = document.createElement("button")
+        btn.className = "delete"
+        btn.innerHTML = 'x'
+        btn.addEventListener('click', () => {
+            let x = Number(btn.id)
+            myShelf.splice(x, 1)
+            console.log(myShelf)
+            bookNum = myShelf.length
+            console.log(bookNum)
+            displayNewShelf()
+        })
+        document.getElementById('book'+i).appendChild(btn)
+    }
+}
+
+function clearForm() {
+    titleVal.value = ''
+    authorVal.value = ''
+    pagesVal.value = ''
+
+}
+
+function verifyForm() {
+    if (titleVal.value == '') {
+        alert("Please add title!")
+    } else if (authorVal.value == '') {
+        alert("Please add author!")
+    } else if (pagesVal.value <= 0) {
+        alert("Please add page count!")
+    } else {
+        addBook()
+    }
+}
+function addBook() {
+    console.log(bookNum)
+    let newBook = new Object()
+    newBook.title = titleVal.value
+    newBook.author = authorVal.value
+    newBook.pages = pagesVal.value
+    newBook.read = readVal.checked
+    myShelf.push(newBook)
+    displayNewShelf()
+    clearForm()
+}
+
+document.getElementById("add-book").addEventListener("click",(event)=>{
+    event.preventDefault();
+})
